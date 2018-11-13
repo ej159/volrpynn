bd57ee3f5fb4277f62eb4f5ff6378a787c07a154
Jens Egholm <jensegholm@protonmail.com>
2018-11-13 16:18:49 +0100

    Corrected default encoding in the docker file

--- volrpynn/__init__.py
+++ volrpynn/__init__.py
@@ -6,11 +6,6 @@ A Python library for training and testing spiking neural network
 models in PyNN.
 """
 
-# Hack to avoid Unicode crash
-import sys
-reload(sys)
-sys.setdefaultencoding('UTF8')
-
 # Import VolrPyNN 
 from . import layer
 from .layer import *
