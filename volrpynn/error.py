c67d53719ac5b190e825500149a9f1c2360bac02
Jens Egholm <jensegholm@protonmail.com>
2018-12-05 23:05:16 +0100

    added argmax sparse

--- volrpynn/error.py
+++ volrpynn/error.py
@@ -2,8 +2,21 @@
 A module for error functions and their derivatives
 """
 import abc
-import numpy
+import numpy as np
 import volrpynn.activation
 
 def sum_squared_error(output, labels):
     return ((output - labels) ** 2).sum()
+
+def argmax_index(xs, randomise_ties=True):
+    max_value = xs.max()
+
+    if max_value == 0:
+        return np.zeros(xs.shape)
+    
+    non_zero_indices = np.flatnonzero(xs == max_value)
+
+    if randomise_ties:
+        return np.random.choice(non_zero_indices)
+    else:
+        return non_zero_indices[0]
