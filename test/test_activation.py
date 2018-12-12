379acfc2e3f772a3a61bf43338333022096c600e
Jens Egholm <jensegholm@protonmail.com>
2018-12-12 11:45:40 +0100

    Added sigmoid derivation

--- test/test_activation.py
+++ test/test_activation.py
@@ -7,3 +7,8 @@ def test_relu_derived():
     expected = np.array([[0], [0], [1]])
     assert np.allclose(actual, expected)
 
+def test_sigmoid_derived():
+    out = np.array([[0], [1], [2]])
+    actual = v.sigmoid_derived(out)
+    expected = np.array([[0], [0], [-2]])
+    assert np.allclose(actual, expected)
--- volrpynn/activation.py
+++ volrpynn/activation.py
@@ -7,3 +7,5 @@ def relu_derived(output):
     output[output < 0] = 0 # ReLU gradient
     return output
 
+def sigmoid_derived(output):
+    return output * (1 - output)
